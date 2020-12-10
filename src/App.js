eda8c166824b3077845243300e008ab847219230
Frank Lai <frank810618@gmail.com>
2020-12-10 10:24:57 +0800

    test basic JSX

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is a React App.
         </p>
       </div>
     );
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I\'m a React App.'));
   }
 }
 
