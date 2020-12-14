0c8b6af57e7c0f8aa0f93a4d9cf0189026ea64ab
Frank Lai <frank810618@gmail.com>
2020-12-14 16:49:59 +0800

    feature: passing methods between components use arrow function or Function.prototype.bind to bind function references

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
     ]
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // This won't work, React would not recognize direct mutation of the state.
     // this.state.persons[0].name = 'Franky';
     this.setState({
       persons: [
-        { name: 'Franky', age: 28 },
+        { name: newName, age: 28 },
         { name: 'John', age: 30 }
       ]
     })
@@ -26,9 +26,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1>This is a React App.</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>I'm a software Engineer.</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I'm a data Engineer.</Person>
+        <button onClick={() => this.switchNameHandler('James')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, 'Kobe')}>I'm a software Engineer.</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}>I'm a data Engineer.</Person>
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I\'m a React App.'));
